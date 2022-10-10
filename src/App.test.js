b441e4b641d1b21fb92ea9a01ee2900795c53be3
minhnp <xiuquay55@gmail.com>
2022-10-11 00:10:56 +0700

    modify code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello circle/i);
+  const linkElement = screen.getByText(/hello minh/i);
   expect(linkElement).toBeInTheDocument();
 });
