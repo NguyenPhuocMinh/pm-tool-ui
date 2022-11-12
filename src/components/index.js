d09d52fca22aa18447519ce938ecc628574ab364
minhnp <xiuquay55@gmail.com>
2022-11-12 15:06:45 +0700

    fix build

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,92 @@
-export * from './commons';
-export * from './buttons';
-export * from './inputs';
-export * from './customs';
-export * from './mains';
-export * from './regulars';
-export * from './sections';
-export * from './settings';
+import AuthCommon from './commons/AuthCommon';
+import LoadingCommon from './commons/LoadingCommon';
+import ErrorCommon from './commons/ErrorCommon';
+import NotifyCommon from './commons/NotifyCommon';
+import TitleCommon from './commons/TitleCommon';
+import MenuCommon from './commons/MenuCommon';
+import NoRowsCommon from './commons/NoRowsCommon';
+import PopupCommon from './commons/PopupCommon';
+import NotFoundCommon from './commons/NotFoundCommon';
+import TabsCommon from './commons/TabsCommon';
+import TabPanelCommon from './commons/TabPanelCommon';
+import TransferListCommon from './commons/TransferListCommon';
+import TypoCommon from './commons/TypoCommon';
+
+import ButtonCreate from './buttons/ButtonCreate';
+import ButtonSubmit from './buttons/ButtonSubmit';
+import ButtonCancel from './buttons/ButtonCancel';
+import ButtonConfirm from './buttons/ButtonConfirm';
+
+import AppBarCustom from './customs/AppBarCustom';
+import DrawerCustom from './customs/DrawerCustom';
+import DrawerHeaderCustom from './customs/DrawerHeaderCustom';
+import DialogCustom from './customs/DialogCustom';
+import DialogTitleCustom from './customs/DialogTitleCustom';
+
+import TextInput from './inputs/TextInput';
+import SearchInput from './inputs/SearchInput';
+import SwitchInput from './inputs/SwitchInput';
+
+import NavLinkMain from './mains/NavLinkMain';
+import ButtonGroupMain from './mains/ButtonGroupMain';
+import ColorPickerMain from './mains/ColorPickerMain';
+
+import LoadingRegular from './regulars/LoadingRegular';
+
+import LanguageSection from './sections/LanguageSection';
+import ProfileSection from './sections/ProfileSection';
+import SettingSection from './sections/SettingSection';
+import NotifySection from './sections/NotifySection';
+import SearchSection from './sections/SearchSection';
+import RefreshSection from './sections/RefreshSection';
+
+import LanguageSetting from './settings/LanguageSetting';
+import CommonSetting from './settings/CommonSetting';
+import ProfileSetting from './settings/ProfileSetting';
+import NotifySetting from './settings/NotifySetting';
+import SearchSetting from './settings/SearchSetting';
+
+export {
+  AuthCommon,
+  LoadingCommon,
+  ErrorCommon,
+  NotifyCommon,
+  TitleCommon,
+  MenuCommon,
+  NoRowsCommon,
+  PopupCommon,
+  NotFoundCommon,
+  TabsCommon,
+  TabPanelCommon,
+  TransferListCommon,
+  TypoCommon,
+  ButtonCreate,
+  ButtonSubmit,
+  ButtonCancel,
+  ButtonConfirm,
+  AppBarCustom,
+  DrawerCustom,
+  DrawerHeaderCustom,
+  DialogCustom,
+  DialogTitleCustom,
+  TextInput,
+  SearchInput,
+  SwitchInput,
+  NavLinkMain,
+  ButtonGroupMain,
+  ColorPickerMain,
+  LoadingRegular,
+  ProfileSection,
+  LanguageSection,
+  SettingSection,
+  NotifySection,
+  SearchSection,
+  RefreshSection,
+  CommonSetting,
+  LanguageSetting,
+  ProfileSetting,
+  NotifySetting,
+  SearchSetting
+};
+
 export * from './utils';
