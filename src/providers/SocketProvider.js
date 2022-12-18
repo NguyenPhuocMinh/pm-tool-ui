758b25ab7323a2ab09c739af271993e34ba86edd
minhnp <xiuquay55@gmail.com>
2022-12-18 19:21:05 +0700

    fix cors

--- src/providers/SocketProvider.js
+++ src/providers/SocketProvider.js
@@ -2,14 +2,14 @@ import { useMemo } from 'react';
 import { SocketContext } from '@context';
 import { useSelector } from 'react-redux';
 import { io } from 'socket.io-client';
-import configs from '@configs';
+// import configs from '@configs';
 
 const SocketProvider = ({ children }) => {
   const _ = useSelector((state) => state);
 
   const socketProvider = useMemo(() => {
     return {
-      socket: io(configs.basePathRestApi)
+      socket: io('https://pm-tool-api.vercel.app/')
     };
   }, []);
 
