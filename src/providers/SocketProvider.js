69f7dadb913d54a4cf978b4bb7e93da4a177075c
minhnp <xiuquay55@gmail.com>
2022-12-19 11:39:11 +0700

    fix build

--- src/providers/SocketProvider.js
+++ src/providers/SocketProvider.js
@@ -2,13 +2,18 @@ import { useMemo } from 'react';
 import { SocketContext } from '@context';
 import { useSelector } from 'react-redux';
 import { io } from 'socket.io-client';
-import configs from '@configs';
+// import configs from '@configs';
 
 const SocketProvider = ({ children }) => {
   const _ = useSelector((state) => state);
 
   const socketProvider = useMemo(() => {
-    const socket = io(configs.basePathRestApi);
+    const socket = io('https://pm-tool-api.vercel.app/', {
+      withCredentials: true,
+      extraHeaders: {
+        'my-custom-header': 'abcd'
+      }
+    });
 
     socket.on('connect', () => {
       console.info('Socket ID', socket.id);
