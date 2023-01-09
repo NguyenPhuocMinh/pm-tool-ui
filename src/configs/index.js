b2aefd9fbca69c5297ee93d570054a67fae9f87c
minhnp <minhnp@mti-tech.vn>
2023-01-10 00:25:26 +0700

    fix header request

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,9 +2,7 @@ const nodeEnv = process.env.NODE_ENV;
 const basePathRestApi = process.env.REACT_APP_REST_API_PROVIDER;
 
 const headers = {
-  'Content-type': 'application/json',
   'Access-Control-Allow-Origin': 'https://pm-tool-api.vercel.app',
-  'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
   'Access-Control-Max-Age': 86400
 };
 
