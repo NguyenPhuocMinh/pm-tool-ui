7edf503cbe428212a066875bce2eae520d3153ea
minhnp <xiuquay55@gmail.com>
2023-01-10 23:42:24 +0700

    fix linter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Provider as ReduxProvider } from 'react-redux';
 import reduxStore, { persistedStore } from '@reduxStore/index';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import { AuthProvider } from '@providers';
+import { AuthProvider, SocketProvider } from '@providers';
 
 import { I18nextProvider } from 'react-i18next';
 import i18nStore from '@i18nStore';
@@ -18,7 +18,9 @@ const App = () => {
         <AuthProvider>
           <I18nextProvider i18n={i18nStore}>
             <BrowserRouter>
-              <Container />
+              <SocketProvider>
+                <Container />
+              </SocketProvider>
             </BrowserRouter>
           </I18nextProvider>
         </AuthProvider>
