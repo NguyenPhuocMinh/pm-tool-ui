9558b6555cd15f90eb92afd4c996f2b295250a9b
minhnp <minhnp@mti-tech.vn>
2023-01-10 11:12:23 +0700

    fix headers

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,7 +2,8 @@ const nodeEnv = process.env.NODE_ENV;
 const basePathRestApi = process.env.REACT_APP_REST_API_PROVIDER;
 
 const headers = {
-  'Access-Control-Allow-Origin': 'https://pm-tool-api.vercel.app',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': basePathRestApi,
   'Access-Control-Allow-Methods':
     'GET, POST, PUT, PATCH, POST, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
